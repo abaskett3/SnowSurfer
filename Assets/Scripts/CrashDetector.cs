7b5415f8245e406bcc0eddc7bda7f9277e65560e
Arland Baskett <10681302+abaskett3@users.noreply.github.com>
2025-11-20 14:28:11 -0800

    Day Three

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    private int sceneIndex = 0;
+    [SerializeField] float reloadSceneDelay = 1f;
+    [SerializeField] ParticleSystem CrashParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
-            Debug.Log("Crash Detected!");
+        {
+            CrashParticles.Play();
+            Invoke("ReloadScene", reloadSceneDelay);
+        }
+
+
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    private int sceneIndex = 0;
+    [SerializeField] float ReloadSceneDelay = 1f;
+
+    [SerializeField] ParticleSystem FinishLineParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if (collision.gameObject.layer == layerIndex)
-            Debug.Log("Finish Line Crossed!");
+        {
+            FinishLineParticles.Play();
+            Invoke("ReloadScene", ReloadSceneDelay);
+        }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
 
